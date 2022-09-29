2d45bda95c8a20da43b4b9d85ac157da77e0fca4
RichardWillianx <richardwilliansp@gmail.com>
2022-09-28 21:22:39 -0300

    Validação de usuário.

--- src/middlewares/validateUser.ts
+++ src/middlewares/validateUser.ts
@@ -4,9 +4,11 @@ import { User } from "../models/user";
 
 export class ValidateUserMiddleware {
   validateUser(request: Request, response: Response, next: NextFunction) {
-    const { userId } = request.params;
+    const { userId, id } = request.params;
 
-    const user = usersApp.find((user: User) => userId === user.id);
+    const user = usersApp.find(
+      (user: User) => userId === user.id || id === user.id
+    );
 
     if (!user) {
       return response.status(404).json({ message: "Usuário não encontrado." });
